5a9f83964cfb22ede92d72798c943c7be10032a9
Carlos Carvajal <carv0066@algonquinlive.com>
2025-07-23 22:24:55 -0400

    More CSS improvements

---
+++ script.js
@@ -0,0 +1,11 @@
+const addItem = document.querySelector(".add-item");
+
+console.log(addItem,"exists!!!");
+
+//Make add button clickable, log clicks
+//when add button is clicked, display content in the task list format example
+//make inpu text disappear after clicking add
+//after clicking on the radio button, add a striketrough
+//the to do list should be saved even after reloading the page (local storage)
+//Make icon on radio button a checkmark
+//Add a clear all button
