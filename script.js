038bb698cf3b1edb49930c14a041ff2b8eaee294
Carlos Carvajal <carv0066@algonquinlive.com>
2025-08-01 18:08:14 -0400

    adding event listener when button is clicked

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 const addItem = document.querySelector(".add-item");
+const inputText  = document.querySelector(".list-input");
+const taskList =  document.querySelector(".task-items");
 
-console.log(addItem,"exists!!!");
+addItem.addEventListener("click", () => {
+    console.log("button has been clicked!!");
+    //Create new items when button is clicked
+    const fragment = document.createDocumentFragment();
+    const p = fragment
+    const newItem = document.createElement("div");
+    newItem.classList.add("item");
+    const newInput = document.createElement("input");
+    newInput.type = "radio";
+    const newLabel = document.createElement("label");
+    newLabel.classList.add("item-text");
+    const deleteIcon = document.createElement("img");
+    deleteIcon.src = "/images/cross-red.png";
+    deleteIcon.alt = "delete";
+    deleteIcon.classList.add("delete-item");
+    p.textContent = inputText.value;//the text written on the input
+    taskList.appendChild(newItem);
+    newItem.appendChild(newInput);
+    newItem.appendChild(newLabel);
+    newLabel.appendChild(fragment);
+    newItem.appendChild(deleteIcon);
+    inputText.value = "";
 
-//Make add button clickable, log clicks
+
+})
+
+
 //when add button is clicked, display content in the task list format example
 //make inpu text disappear after clicking add
 //after clicking on the radio button, add a striketrough
