744e0f6837ca398a837ea5234856bfa64dffbf67
Carlos Carvajal <carv0066@algonquinlive.com>
2025-08-08 00:23:47 -0400

    worked on checkboxes

--- script.js
+++ script.js
@@ -8,100 +8,127 @@ const taskList = document.createElement("div");
 taskList.classList.add("task-items");
 tasksDiv.appendChild(taskList);
 
-function createTaskElement() {
-    //If the value of the input isn't empty then add a to do list item
+
+const parsedTasks = JSON.parse(localStorage.getItem("savedItem") || "[]"); //replaced hardcoded array with the parsedTasks
+console.log("parsed Tasks", parsedTasks);
+
+
+parsedTasks.forEach(item => {
+    console.log("item", item.text);
     const newItem = document.createElement("div");
     newItem.classList.add("item");
 
     const inputContainer = document.createElement("div");
     inputContainer.classList.add("input-text");
 
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.classList.add("checkbox");
+    const createCheckbox = document.createElement("input");
+    createCheckbox.type = "checkbox";
+    createCheckbox.classList.add("checkbox");
 
     const p = document.createElement("p");
     p.classList.add("item-text");
-    p.textContent = inputText.value; //the text written on the input
+    p.textContent = item.text; //the text inside the array
 
     const deleteIcon = document.createElement("img");
     deleteIcon.src = "/images/cross-red.png";
     deleteIcon.alt = "delete";
     deleteIcon.classList.add("delete-item");
 
-    inputContainer.appendChild(checkbox);
+    inputContainer.appendChild(createCheckbox);
     inputContainer.appendChild(p);
     newItem.appendChild(inputContainer);
     newItem.appendChild(deleteIcon);
+    taskList.appendChild(newItem);
+});
 
-    return newItem; 
-}
-
-
-const parsedTasks = JSON.parse(localStorage.getItem("savedItem") || "[]");//replaced hardcoded array with the parsedTasks
-
-window.addEventListener("DOMContentLoaded", () => {
-    parsedTasks.forEach(item => {
-        console.log("item", item);
-        const taskElement = createTaskElement(item);
-        taskList.appendChild(taskElement);//Element wil now show up even after reloading page
-
-
-    });
-})
 //Add to do list Item
 addItem.addEventListener("click", () => {
     //Create new items when button is clicked
     let text = inputText.value;
-
     if (text) {
-        const itemData = { text, checked: true };
-        const taskElement = createTaskElement(itemData);
-        taskList.appendChild(taskElement);
+        itemData = {
+            text,
+            checked: false
+        };
+        //If the value of the input isn't empty then add a to do list item
+        const newItem = document.createElement("div");
+        newItem.classList.add("item");
 
-        parsedTasks.push(itemData);//I was pushing them to tasks before and thats why it wasn't saving, it was rewriting my local storage everytime
-        console.log("tasks",parsedTasks);
+        const inputContainer = document.createElement("div");
+        inputContainer.classList.add("input-text");
 
-        localStorage.setItem("savedItem", JSON.stringify(parsedTasks));//Replaced the hardcoded tasks from here too
+        const createCheckbox = document.createElement("input");
+        createCheckbox.type = "checkbox";
+        createCheckbox.classList.add("checkbox");
 
-        inputText.value = "";//Issue with this at the moment, the input is not clearing up
+        const p = document.createElement("p");
+        p.classList.add("item-text");
+        p.textContent = inputText.value; //the text written on the input
 
-    }})
+        const deleteIcon = document.createElement("img");
+        deleteIcon.src = "/images/cross-red.png";
+        deleteIcon.alt = "delete";
+        deleteIcon.classList.add("delete-item");
+
+        inputContainer.appendChild(createCheckbox);
+        inputContainer.appendChild(p);
+        newItem.appendChild(inputContainer);
+        newItem.appendChild(deleteIcon);
+        taskList.appendChild(newItem);
 
 
-    //     //Add a striketrough whenever the button is clicked 
-    //     checkbox.addEventListener("click", () => {
-    //         localStorage.setItem("checkboxState", checkbox.checked);
-    //         console.log('Checkbox state saved:', checkbox.checked);
+        //I need to push the data and then find  modify it
+        parsedTasks.push(itemData);
+        console.log("item date being pushed", itemData);
+        let propertyToChange = "checked";
+        localStorage.setItem("savedItem", JSON.stringify(parsedTasks));
 
-    //         const savedState = localStorage.getItem("checkboxState")
+        createCheckbox.addEventListener("click", () => {
+            itemData.checked = createCheckbox.checked;
+            parsedTasks[propertyToChange] = itemData.checked;
+            console.log("the checked value is", itemData.checked);
+            localStorage.setItem("savedItem", JSON.stringify(parsedTasks));
+        }) 
+        //I need to now save the checkbox checked value to be saved when reloading the page,is saved correctly in the local storage but its not being saved correctly when it comes to displaying it
 
-    //         if (savedState !== null) { //Checks if something was saved in the storage before
-    //             checkbox.checked = (savedState === "true");
-    //             console.log('Checkbox state loaded:', checkbox.checked);
-    //         }
 
-    //         if (checkbox.checked) {
-    //             newItem.style.textDecoration = "line-through";
-    //             newLabel.style.color = "#8f8b8bc8";
-    //         } else {
-    //             newItem.style.textDecoration = "none";
-    //             newLabel.style.color = "#0b2540";
-    //         }
-    //     })
+        inputText.value = "";
 
-    //     //Delete item from list
-    //     deleteIcon.addEventListener("click", () => {
-    //         console.log("item has been deleted");
-    //         newItem.remove();
-    //     })
+    }
+})
+//work on checked button with states of true or false
+//work on delete button
+//work on clear all button
+//Work on score to track completed, and deleted, and maybe it gets reset with a button option for that
+//shorten code, create functions and remove repeated code
 
-    // } else {
-    //     console.log("input is empty");
-    // }
-//I would need to save the item, whether the item has been completed, and whehter the item has been deleted, three things.
+// const checkbox = document.querySelectorAll(".checkbox");
 
-//the to do list should be saved even after reloading the page (local storage) same with the completed and deleted
+// checkbox.forEach(box => {
+//     box.addEventListener("change", () => {
+//         itemData.checked = box.checked;
+//         console.log("checkboxState", box.checked);
+//         console.log("item data 2", itemData.checked);
+//         localStorage.setItem("checkbox", box.checked);
+//         //currently it only works after reoading the page, so if i add an item imediatly it doesn't really log it in the storage
+//         //currently item date 1 and item data 2 can't be checked at the same time and i need that
+//         //Also this is a good try but I'm still doing it wrong, I already have a local storage where im saving both the text and the checked, i need to update that not a separate key
+//     })
+
+// });
+
+
+//     //Delete item from list
+//     deleteIcon.addEventListener("click", () => {
+//         console.log("item has been deleted");
+//         newItem.remove();
+//     })
+
+// } else {
+//     console.log("input is empty");
+// }
+
+
 //add in corner: completed and deleted
 //Make icon on radio button a checkmark(use images)
 //Add a clear all button
